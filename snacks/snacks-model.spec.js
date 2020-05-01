40b38ba1023c6300c7743f7243f0a640f28a426d
oashtari <oashtari@gmail.com>
2020-05-01 07:33:02 -0700

    finished delete testing

--- snacks/snacks-model.spec.js
+++ snacks/snacks-model.spec.js
@@ -1,23 +1,7 @@
 const request = require("supertest")
 const server = require('../api/server')
 const db = require('../data/db-config');
-
 const Snacks = require('./snacks-model');
-
-// describe('POST /users', function () {
-//     it('responds with json', function (done) {
-//         request(app)
-//             .post('/users')
-//             .send({ name: 'john' })
-//             .set('Accept', 'application/json')
-//             .expect('Content-Type', /json/)
-//             .expect(200)
-//             .end(function (err, res) {
-//                 if (err) return done(err);
-//                 done();
-//             });
-//     });
-// });
 
 describe('snacks model', () => {
     describe('insert()', () => {
@@ -25,12 +9,6 @@ describe('snacks model', () => {
         beforeEach(async () => {
             await db('snacks').truncate();
         })
-
-        // it('should return 200 ok', async function () {
-        //     const response = await request(server).get('/');
-        //     console.log('response', response.status)
-        //     expect(response.status).toBe(200)
-        // })
 
         it('should return 200 ok', function () {
             return request(server)
@@ -68,41 +46,19 @@ describe('snacks model', () => {
             // console.log('snacks', newSnacks)
             expect(newSnacks).toHaveLength(1)
         })
+
+        it('should return a JSON', function () {
+            return request(server)
+                .delete('/:id')
+                .then(response => {
+                    expect(response.type).toMatch(/text/i)
+                })
+        })
+        it('should return a 404', function () {
+            return request(server)
+                .delete('/12')
+                .send({ name: "orange", count: 6 })
+                .then(response => expect(response.status).toBe(404))
+        })
     })
 })
-
-
-
-
-
-
-// const request = require("supertest");
-// const server = require("../api/server");
-
-// describe("dogsRouter.js", function () {
-
-
-
-
-//     describe("DELETE /:id", function () {
-//         it("should return a Json", function () {
-//             return request(server) //make sure to put return
-//                 .delete("/:id")
-
-//                 .then(res => {
-//                     expect(res.type).toMatch(/text/i);
-//                 });
-//         });
-//         it("should return a 404", function () {
-//             return request(server) //make sure to put return
-//                 .delete("/9")
-//                 .send({
-//                     name: "andrew",
-//                     breed: "dog"
-//                 })
-//                 .then(res => {
-//                     expect(res.status).toBe(404);
-//                 });
-//         });
-//     });
-// });
